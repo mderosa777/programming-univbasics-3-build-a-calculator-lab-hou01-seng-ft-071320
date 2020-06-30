ceaa0fc963fc1317889a8fd9c29f5f61149e1e5b
megan <meggsnbacon777@gmail.com>
2020-06-30 05:01:45 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,49 @@
 def addition(num1, num2)
-
+puts "i got#{num1}"
+puts "i got#{num2}"
+sum = num1 + num2
+puts"i got #{sum}"
+sum 
 end
 
 def subtraction(num1, num2)
-
+puts "i got#{num1}"
+puts "i got#{num2}"
+sum = num1 - num2
+puts "i got #{sum}"
+sum 
 end
 
 def division(num1, num2)
+  puts "i got#{num1}"
+puts "i got#{num2}"
+sum = num1 / num2
+puts "i got #{sum}"
+sum 
 
 end
 
 def multiplication(num1, num2)
-
+puts "i got#{num1}"
+puts "i got#{num2}"
+sum = num1 * num2
+puts "i got #{sum}"
+sum 
 end
 
 def modulo(num1, num2)
-
+puts "i got#{num1}"
+puts "i got#{num2}"
+sum = num1 % num2
+puts "i got #{sum}"
+sum 
 end
 
 def square_root(num)
+  puts"igot #{num}" 
+  sum = Math.sqrt(num)
+  sum
+  
+ 
 
 end
